5f00a46878a92b5a98c0edcdbcdec1af900659c2
xJam-byte <abylaimuratbayev@gmail.com>
2025-01-17 11:09:04 +0500

    first commit

---
+++ src/main/java/com/example/endterm_project/EndtermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.endterm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndtermProjectApplication.class, args);
+    }
+
+}
